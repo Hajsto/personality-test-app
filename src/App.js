d8bc01762249390c234c7bc30b03e61181fc87db
Hajsto <45203299+Hajsto@users.noreply.github.com>
2024-10-05 10:43:40 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import PersonalityTest from './components/PersonalityTest';
+import Result from './components/Result';
+import questions from './data/questions';
+import analyzePersonality from './utils/analyzePersonality';
+
+const App = () => {
+  // ... (zbytek kódu App komponenty)
+};
+
+export default App;
